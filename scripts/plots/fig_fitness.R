a8f2647723462c2470971a0522fc057bfac7ca00
rshuhuachen <rschen96@gmail.com>
2024-10-01 08:54:35 +0200

    update plots

--- scripts/plots/fig_fitness.R
+++ scripts/plots/fig_fitness.R
@@ -38,6 +38,7 @@ ggplot(delta_out_ams, aes(x = ams_delta_meth_estimate, y = -log10(ams_delta_meth
     geom_point(size=7, alpha=0.5, aes(col = sig, fill = sig)) +
     scale_color_manual(values=c(clrs[5], clrs[17])) +
     scale_fill_manual(values=alpha(c(clrs[5], clrs[17]), 0.6)) +
+    xlim(-21, 21)+
     labs(x = expression("Estimate "*Delta*" methylation"), y = "-log10(q-value)", title = "Annual mating success") +
     geom_hline(yintercept = -log10(0.05), col = "darkred", linetype = "dotted", linewidth = 1) +
     geom_vline(xintercept = 0, col = "darkred", linetype = "dotted", linewidth = 1) +
@@ -82,7 +83,8 @@ ggplot(delta_out_surv, aes(x = surv_delta_meth_estimate, y = -log10(surv_delta_m
     scale_color_manual(values=c(clrs[5], clrs[17])) +
     scale_fill_manual(values=alpha(c(clrs[5], clrs[17]), 0.6)) +
     labs(x = expression("Estimate "*Delta*" methylation"), y = "-log10(q-value)") +
-   geom_hline(yintercept = -log10(0.05), col = "darkred", linetype = "dotted", linewidth = 1) +
+    xlim(-42, 42)+
+    geom_hline(yintercept = -log10(0.05), col = "darkred", linetype = "dotted", linewidth = 1) +
     geom_vline(xintercept = 0, col = "darkred", linetype = "dotted", linewidth = 1) +
     theme(legend.position="none") -> volcano_surv
 
@@ -90,21 +92,15 @@ ggsave(volcano_surv, file = "plots/test.png", width=10, height=10)
 
 ### significant ones
 delta_out_surv <- delta_out_surv %>% arrange(surv_delta_meth_qval)
+
 list_plot_surv <- list()
 for (i in 1:2){
         sub <- subset(delta_meth, chr_pos == delta_out_surv$chr_pos[[i]] & !is.na(delta_meth) & !is.na(surv))
         
-        model <- glmmTMB::glmmTMB(surv ~ delta_meth + (1|site/id), family = "binomial",
-                            data = sub)
-                            
-        gr <- ref_grid(model, cov.keep= c('delta_meth'))
-        predict <- as.data.frame(emmeans(gr, spec="delta_meth", level=0.95))
-        
-        ggplot(sub, aes(x = delta_meth, y = as.numeric(surv))) + 
-                geom_ribbon(data= predict, aes(ymin = asymp.LCL, ymax = asymp.UCL, y= NULL), fill= clrs[5], alpha = 0.6) +
-                geom_line(data= predict, aes(y = emmean), col = "black", linewidth=1.5) + 
-                geom_point(size = 7, fill = clr_high, alpha = 0.6, col = clr_high) + 
-                labs(x = "Annual mating success", x = expression(Delta*" methylation %")) -> plot
+        ggplot(sub, aes(x = delta_meth, y = surv)) + 
+        geom_boxplot(lwd=1) + 
+        geom_point(size = 7, fill = clr_high, alpha = 0.6, col = clr_high, position=position_jitter(height=0.1)) + 
+        labs(y = "Survival", x = expression(Delta*" methylation %")) -> plot
 
         ggsave(plot, file= paste0("plots/model_out/fitness/raw_surv_", i, ".png"), width=8, height=8)
 
@@ -116,7 +112,7 @@ ggsave(plot, file = "plots/test.png", width=10, height=10)
 ### assemble #####
 
 plot_grid(volcano_ams, list_plot_ams[[1]], 
-          volcano_surv, list_plot_surv[[1]],
+          volcano_surv, list_plot_surv[[1]], align="vh", axis="lb",
           ncol=2, labels="auto", label_fontface = "plain", label_size = 22) -> fig
 
 ggsave(fig, file="plots/final/main/fig_fitness.png", width=16, height=18)
